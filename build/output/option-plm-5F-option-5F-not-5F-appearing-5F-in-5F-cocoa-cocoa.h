765b5cd2850da35ebdeeab407f115f8c89c4d57e
Pierre Molinaro <pierre@pcmolinaro.name>
2017-12-28 12:36:30 +0000

    Début tableaux dynamiques

---
+++ build/output/option-plm-5F-option-5F-not-5F-appearing-5F-in-5F-cocoa-cocoa.h
@@ -0,0 +1,28 @@
+//--- START OF USER ZONE 1
+
+
+//--- END OF USER ZONE 1
+#ifndef plm_option_not_appearing_in_cocoa_FOR_COCOA_DEFINED
+#define plm_option_not_appearing_in_cocoa_FOR_COCOA_DEFINED
+
+//---------------------------------------------------------------------------------------------------------------------*
+
+#import <Cocoa/Cocoa.h>
+
+//---------------------------------------------------------------------------------------------------------------------*
+
+//--- START OF USER ZONE 2
+
+
+//--- END OF USER ZONE 2
+
+//---------------------------------------------------------------------------------------------------------------------*
+
+void enterOptionsFor_plm_5F_option_5F_not_5F_appearing_5F_in_5F_cocoa (NSMutableArray * ioBoolOptionArray,
+                                                                       NSMutableArray * ioUIntOptionArray,
+                                                                       NSMutableArray * ioStringOptionArray,
+                                                                       NSMutableArray * ioStringListOptionArray) ;
+
+//---------------------------------------------------------------------------------------------------------------------*
+
+#endif
