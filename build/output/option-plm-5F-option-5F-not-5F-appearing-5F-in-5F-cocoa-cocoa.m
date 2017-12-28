fbabaeb0948a4bed1c321afb1626e54aa6c9ded9
pm <pm@981b75ee-b715-4e12-89b7-fa3ccf2b03e8>
2017-12-28 12:36:30 +0000

    Début tableaux dynamiques

---
+++ build/output/option-plm-5F-option-5F-not-5F-appearing-5F-in-5F-cocoa-cocoa.m
@@ -0,0 +1,35 @@
+//--- START OF USER ZONE 1
+
+
+//--- END OF USER ZONE 1
+//---------------------------------------------------------------------------------------------------------------------*
+
+#import "option-plm-5F-option-5F-not-5F-appearing-5F-in-5F-cocoa-cocoa.h"
+#import "OC_GGS_CommandLineOption.h"
+
+//---------------------------------------------------------------------------------------------------------------------*
+
+void enterOptionsFor_plm_5F_option_5F_not_5F_appearing_5F_in_5F_cocoa (NSMutableArray * ioBoolOptionArray,
+                                                                       NSMutableArray * ioUIntOptionArray,
+                                                                       NSMutableArray * ioStringOptionArray,
+                                                                       NSMutableArray * ioStringListOptionArray) {
+  OC_GGS_CommandLineOption * option ;
+  option = [[OC_GGS_CommandLineOption alloc]
+    initWithDomainName:@"plm_option_not_appearing_in_cocoa"
+    identifier:@"performFlashing"
+    commandChar:102
+    commandString:@"flash-target"
+    comment:@"After successful compilation, flash target"
+    defaultValue:@""
+  ] ;
+  [ioBoolOptionArray addObject:option] ;
+}
+
+//---------------------------------------------------------------------------------------------------------------------*
+
+//--- START OF USER ZONE 2
+
+
+//--- END OF USER ZONE 2
+
+
